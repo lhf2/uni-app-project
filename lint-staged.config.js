230f3bf19bcc03637bcbc89c40844a8f0c0b275c
lhf <626712309@qq.com>
2022-07-05 15:24:57 +0800

    fix: 🐛 修改代码规范配置
    修改代码规范配置

--- lint-staged.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    '*.{js,jsx,ts,tsx,vue}': ['eslint --fix', 'prettier --write'],
-    '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['prettier --write--parser json'],
-    'package.json': ['prettier --write'],
-    '*.md': ['prettier --write']
-}
