51dbcf90cce45161a63b7e10fb911e71c30082da
lhf <626712309@qq.com>
2022-07-06 11:08:31 +0800

    fix: 🐛 pages.json忽略代码检查

--- src/components/login.vue
+++ src/components/login.vue
@@ -8,106 +8,104 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex'
-export default {
-  name: 'login',
-  data() {
-    return {
-      code: ''
-    }
-  },
-  created() {
-    this.getCode()
-  },
-  computed: {
-    isLogin() {
-      return this.$store.state.isLogin
-    }
-  },
-  methods: {
-    ...mapActions([
-      'setLogin'
-    ]),
-    getCode() {
-      const _self = this
-      uni.login({
-        provider: 'weixin',
-        success(res) {
-          _self.code = res.code
-        },
-      })
+  import { mapActions } from 'vuex'
+  export default {
+    name: 'login',
+    data() {
+      return {
+        code: '',
+      }
     },
-    getPhoneNumber(e) {
-      const _self = this
-      // 不允许授权
-      if (e.detail.errMsg != 'getPhoneNumber:ok') {
-        uni.showToast({
-          title: '获取手机号失败',
-          icon: 'none',
+    created() {
+      this.getCode()
+    },
+    computed: {
+      isLogin() {
+        return this.$store.state.isLogin
+      },
+    },
+    methods: {
+      ...mapActions(['setLogin']),
+      getCode() {
+        const _self = this
+        uni.login({
+          provider: 'weixin',
+          success(res) {
+            _self.code = res.code
+          },
         })
-        return
-      }
-      // 允许授权 传给后端解析
-      if (!_self.code) {
-        _self.getCode()
-        return
-      }
-
-      let params = {
-        login_code: _self.code,
-        iv: e.detail.iv,
-        encrypted_data: e.detail.encryptedData,
-      }
-
-      _self.getminiProgramToken(params)
-    },
-    //服务端解密手机号
-    getminiProgramToken(obj) {
-      const _self = this
-      // todo 调用后端接口，成功了之后做下面的操作
-      uni.setStorage({
-        key: 'token',
-        data: 'this is a token',
-        success: function () {
+      },
+      getPhoneNumber(e) {
+        const _self = this
+        // 不允许授权
+        if (e.detail.errMsg != 'getPhoneNumber:ok') {
           uni.showToast({
-            title: '授权成功',
+            title: '获取手机号失败',
             icon: 'none',
-            duration: 1500,
           })
-          _self.setLogin(true)
-          _self.handleCb()
-        },
-      })
+          return
+        }
+        // 允许授权 传给后端解析
+        if (!_self.code) {
+          _self.getCode()
+          return
+        }
+
+        let params = {
+          login_code: _self.code,
+          iv: e.detail.iv,
+          encrypted_data: e.detail.encryptedData,
+        }
+
+        _self.getminiProgramToken(params)
+      },
+      //服务端解密手机号
+      getminiProgramToken(obj) {
+        const _self = this
+        // todo 调用后端接口，成功了之后做下面的操作
+        uni.setStorage({
+          key: 'token',
+          data: 'this is a token',
+          success: function () {
+            uni.showToast({
+              title: '授权成功',
+              icon: 'none',
+              duration: 1500,
+            })
+            _self.setLogin(true)
+            _self.handleCb()
+          },
+        })
+      },
+      handleCb() {
+        if (!this.isLogin) {
+          return
+        }
+        // 触发一个事件
+        this.$emit('loginCb')
+      },
     },
-    handleCb() {
-      if (!this.isLogin) {
-        return
-      }
-      // 触发一个事件
-      this.$emit('loginCb')
-    },
-  },
-}
+  }
 </script>
 
 <style>
-.login-container {
-  width: 100%;
-  height: 100%;
-}
+  .login-container {
+    width: 100%;
+    height: 100%;
+  }
 
-.login-container button {
-  width: 100%;
-  height: 100%;
-  padding: 0;
-  margin: 0;
-  border: 0;
-  background: transparent;
-  border-radius: 0;
-  color: inherit;
-}
+  .login-container button {
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+    border: 0;
+    background: transparent;
+    border-radius: 0;
+    color: inherit;
+  }
 
-.login-container button::after {
-  border: none;
-}
+  .login-container button::after {
+    border: none;
+  }
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -23,7 +23,6 @@
           <text class="zan">👍🏻</text>
           <text class="num">0</text>
         </login>
-
       </view>
     </view>
     <login class="score-btn" @loginCb="handleScore">去打分</login>
@@ -31,119 +30,119 @@
 </template>
 
 <script>
-import { getTaskLists } from '../../api/task'
-import login from '../../components/login.vue'
+  import { getTaskLists } from '../../api/task'
+  import login from '../../components/login.vue'
 
-export default {
-  data() {
-    return {
-      title: 'Hello',
-      newlists: [
-        { id: 1, title: '这是新闻1' },
-        { id: 2, title: '这是新闻2' },
-        { id: 3, title: '这是新闻3' }
-      ]
-    }
-  },
-  components: {
-    login,
-  },
-  onLoad() {
-    // this.getTaskList({
-    // 	page: 1,
-    // 	pageSize: 10
-    // })
-  },
-  methods: {
-    goWeixin1() {
-      uni.navigateTo({
-        url: '../weixin1/weixin1',
-      })
+  export default {
+    data() {
+      return {
+        title: 'Hello',
+        newlists: [
+          { id: 1, title: '这是新闻1' },
+          { id: 2, title: '这是新闻2' },
+          { id: 3, title: '这是新闻3' },
+        ],
+      }
     },
-    goWeixin2() {
-      uni.navigateTo({
-        url: '../weixin2/weixin2',
-      })
+    components: {
+      login,
     },
-    getTaskList(params) {
-      getTaskLists(params).then((res) => console.log('res', res))
+    onLoad() {
+      // this.getTaskList({
+      // 	page: 1,
+      // 	pageSize: 10
+      // })
     },
-    handleScore() {
-      console.log('handleScore 打分')
+    methods: {
+      goWeixin1() {
+        uni.navigateTo({
+          url: '../weixin1/weixin1',
+        })
+      },
+      goWeixin2() {
+        uni.navigateTo({
+          url: '../weixin2/weixin2',
+        })
+      },
+      getTaskList(params) {
+        getTaskLists(params).then((res) => console.log('res', res))
+      },
+      handleScore() {
+        console.log('handleScore 打分')
+      },
+      handleZan(item, index) {
+        console.log('点赞', item, index)
+      },
     },
-    handleZan(item, index) {
-      console.log('点赞', item, index)
-    }
-  },
-}
+  }
 </script>
 
 <style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
+  .logo {
+    height: 200rpx;
+    width: 200rpx;
+    margin-top: 200rpx;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 50rpx;
+  }
 
-.text-area {
-  display: flex;
-  justify-content: center;
-}
+  .text-area {
+    display: flex;
+    justify-content: center;
+  }
 
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
+  .title {
+    font-size: 36rpx;
+    color: #8f8f94;
+  }
 
-.score-btn {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: #ff8008;
-  width: 240rpx;
-  height: 88rpx;
-  position: fixed;
-  bottom: 60rpx;
-  left: 50%;
-  -webkit-transform: translateX(-50%);
-  transform: translateX(-50%);
-  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.204592);
-  border-radius: 44rpx;
-  font-weight: 600;
-  font-size: 32rpx;
-  color: #ffffff;
-  z-index: 21;
-  text-align: center;
-  line-height: 88rpx;
-}
+  .score-btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #ff8008;
+    width: 240rpx;
+    height: 88rpx;
+    position: fixed;
+    bottom: 60rpx;
+    left: 50%;
+    -webkit-transform: translateX(-50%);
+    transform: translateX(-50%);
+    box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.204592);
+    border-radius: 44rpx;
+    font-weight: 600;
+    font-size: 32rpx;
+    color: #ffffff;
+    z-index: 21;
+    text-align: center;
+    line-height: 88rpx;
+  }
 
-.news-lists {
-  margin-top: 100rpx;
-  width: 100%;
-  height: 40px;
-}
+  .news-lists {
+    margin-top: 100rpx;
+    width: 100%;
+    height: 40px;
+  }
 
-.news-item {
-  width: 100%;
-  height: 100%;
-  border-bottom: 1px solid #ff8008;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-}
+  .news-item {
+    width: 100%;
+    height: 100%;
+    border-bottom: 1px solid #ff8008;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
 
-.zan-btn .num {
-  margin-left: 10px;
-  color: #ff8008;
-}
+  .zan-btn .num {
+    margin-left: 10px;
+    color: #ff8008;
+  }
 </style>
