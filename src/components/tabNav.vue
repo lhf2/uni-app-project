7441568ea3215b7a8fcd4a8bc2fe1f0e4266dd15
lhf <626712309@qq.com>
2022-07-08 18:37:08 +0800

    fix: 🐛 修复切换tab时暂停视频的问题

--- src/components/tabNav.vue
+++ src/components/tabNav.vue
@@ -19,75 +19,75 @@
 </template>
 
 <script>
-export default {
-  name: 'tabNav',
-  data() {
-    return {
-      tabList: [
-        { label: '帖子', value: 0 },
-        { label: '热帖', value: 1 },
-        { label: 'PK赛', value: 2 },
-        { label: '明星设计师', value: 3 },
-      ],
-      current: 0,
-    }
-  },
-  onLoad() {
-    let vm = this
-    uni.$on('initData', ({ tabList, current }) => {
-      console.log(tabList)
-      vm.tabList = tabList
-      vm.current = current
-    })
-  },
-}
+  export default {
+    name: 'tabNav',
+    data() {
+      return {
+        tabList: [
+          { label: '帖子', value: 0 },
+          { label: '热帖', value: 1 },
+          { label: 'PK赛', value: 2 },
+          { label: '明星设计师', value: 3 },
+        ],
+        current: 0,
+      }
+    },
+    onLoad() {
+      let vm = this
+      uni.$on('initData', ({ tabList, current }) => {
+        console.log(tabList)
+        vm.tabList = tabList
+        vm.current = current
+      })
+    },
+  }
 </script>
 
 <style>
-.tab-wrapper {
-  width: 100%;
-  height: 45px;
-  display: flex;
-  position: fixed;
-  top: 0;
-  background: #fff;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  border-bottom: 1px solid #efefef;
-}
+  .tab-wrapper {
+    width: 100%;
+    height: 45px;
+    display: flex;
+    position: fixed;
+    top: 0;
+    background: #fff;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 1px solid #efefef;
+  }
 
-.tab-wrapper .item {
-  flex: 1;
-  height: 100%;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  .tab-wrapper .item {
+    flex: 1;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
 
-.tab-wrapper .item .text-box .nav-text {
-  height: 45px;
-  line-height: 45px;
-  font-size: 16px;
-  color: #333333;
-}
+  .tab-wrapper .item .text-box .nav-text {
+    height: 45px;
+    line-height: 45px;
+    font-size: 16px;
+    color: #333333;
+  }
 
-.tab-wrapper .item .text-box.active .nav-text {
-  color: #ff8008;
-  display: inline-block;
-  height: 100%;
-  position: relative;
-}
+  .tab-wrapper .item .text-box.active .nav-text {
+    color: #ff8008;
+    display: inline-block;
+    height: 100%;
+    position: relative;
+  }
 
-.tab-wrapper .item .text-box.active .nav-text::after {
-  content: '';
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 3px;
-  background: #ff8008;
-  border-top-left-radius: 31px;
-  border-top-right-radius: 31px;
-}
+  .tab-wrapper .item .text-box.active .nav-text::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 3px;
+    background: #ff8008;
+    border-top-left-radius: 31px;
+    border-top-right-radius: 31px;
+  }
 </style>
---
+++ src/pages/tasks/tasks.vue
@@ -0,0 +1,31 @@
+<template>
+	<view>
+		这是任务页面
+		这是任务页面
+		这是任务页面
+		这是任务页面
+		这是任务页面
+		这是任务页面
+		这是任务页面
+		这是任务页面
+		这是任务页面
+		这是任务页面
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
